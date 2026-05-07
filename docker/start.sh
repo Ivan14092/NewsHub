63dc7206ba965332624db7014f7a833b51e9c5bc
Ivan Bazyuk <bazyuk2001@gmail.com>
2026-05-07 16:20:22 +0300

    fix:  start script

--- docker/start.sh
+++ docker/start.sh
@@ -4,8 +4,8 @@ set -e
 #echo "Clearing cache..."
 #php bin/console cache:clear --env=prod --no-debug
 
-echo "Running migrations..."
-php bin/console doctrine:migrations:migrate --no-interaction --env=prod
+#echo "Running migrations..."
+#php bin/console doctrine:migrations:migrate --no-interaction --env=prod
 
 echo "Starting PHP-FPM..."
 php-fpm -D
