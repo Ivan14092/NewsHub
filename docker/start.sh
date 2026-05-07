f60a88acc28338979e1e2f68342538703306dfb6
Ivan Bazyuk <bazyuk2001@gmail.com>
2026-05-07 16:07:30 +0300

    fix: comment out cache clear in start script

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Clearing cache..."
-php bin/console cache:clear --env=prod --no-debug
+#echo "Clearing cache..."
+#php bin/console cache:clear --env=prod --no-debug
 
 echo "Running migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction --env=prod
