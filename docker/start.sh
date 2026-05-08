fc7b125c92c56ba42835d580ac0e6f498e2ecf51
Ivan Bazyuk <bazyuk2001@gmail.com>
2026-05-08 11:43:12 +0300

    fix: add sleep before nginx start

--- docker/start.sh
+++ docker/start.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e
 
-#echo "Clearing cache..."
-#php bin/console cache:clear --env=prod --no-debug
-
-#echo "Running migrations..."
-#php bin/console doctrine:migrations:migrate --no-interaction --env=prod
-
 echo "Starting PHP-FPM..."
 php-fpm -D
 
+# Чекаємо поки PHP-FPM запуститься
+sleep 2
+
 echo "Starting Nginx..."
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
