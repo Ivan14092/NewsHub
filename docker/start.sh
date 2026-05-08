def3df507b41343d1d40ea3afa137e60ed5c1955
Ivan Bazyuk <bazyuk2001@gmail.com>
2026-05-08 12:22:39 +0300

    fix: fix var directory permissions for EasyAdmin

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,11 @@ set -e
 echo "Running migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction --env=prod
 
+echo "Fixing permissions..."
+mkdir -p var/cache/prod/easyadmin
+chown -R www-data:www-data var/
+chmod -R 777 var/
+
 echo "Starting PHP-FPM..."
 php-fpm -D
 
