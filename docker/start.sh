391eaaad95c589d2c80ab1ad110f58c1a92d1a59
Ivan Bazyuk <bazyuk2001@gmail.com>
2026-05-08 12:19:05 +0300

    fix: enable migrations in start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "Running migrations..."
+php bin/console doctrine:migrations:migrate --no-interaction --env=prod
+
 echo "Starting PHP-FPM..."
 php-fpm -D
 
-# Чекаємо поки PHP-FPM запуститься
 sleep 2
 
 echo "Starting Nginx..."
